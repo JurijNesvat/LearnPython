6f76e96b95be590babdbaa86be2b5c8390e8c8e8
Jurij Nesvat <nesvod@gmail.com>
2016-03-05 10:43:21 +0200

    Data Structures

---
+++ 2 - Data Structures/Week 1 - Strings.py
@@ -0,0 +1,31 @@
+#str1 = "afff"
+#str2 = "gbggg"
+#bob = str1 + str2
+
+#print bob
+
+
+
+"""
+letter = str1[0]
+letter2 = str2[1]
+
+print letter + letter2
+
+for letter in "banana":
+    print letter
+
+s = "Laurencija"
+print s[0:4]
+"""
+
+#Assignement:
+#text = "X-DSPAM-Confidence:    0.8475";
+
+#strstart = text.find("0")
+#strend = text.find("5")
+
+#CroppedString = text[strstart:strend]
+
+#StringAsFloat = float(CroppedString)
+#print StringAsFloat
